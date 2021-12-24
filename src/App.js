2305f1ea0f8679fa252db5691eead2b31847943f
uwadonat <uwadonat@gmail.com>
2021-12-24 14:53:24 +0300

    display object

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import React, { Component } from "react";
 class App extends Component {
   name = "Donat";
   render() {
+    const styles = {
+      border: "solid",
+      width: "200px",
+    };
+    const names = {
+      name1: "donat",
+    };
+
     return (
       <div>
+        <div style={styles}>hi</div>
         <h1>Hello {this.name}</h1>
         <button>Hover me</button>
+        <div>{names.name1}</div>
       </div>
     );
   }
