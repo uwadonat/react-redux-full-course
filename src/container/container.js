31d42dbebd08f785a137ad1d827ea82b6d4e2b49
uwadonat <uwadonat@gmail.com>
2021-12-25 20:12:12 +0300

    setState

--- src/container/container.js
+++ src/container/container.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class Container1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: "34",
+    };
+  }
+
+  //wrong
+  //   changeState = () => {
+  //     this.state.age = "200";
+  //   };
+
+  //correct
+  changeState = () => {
+    this.setState({ age: "200" });
+  };
   render() {
-    return <div>{this.props.Nickname}</div>;
+    return (
+      <div>
+        <button onClick={() => this.changeState()}>Change state</button>
+        <div>{this.state.age}</div>
+      </div>
+    );
   }
 }
 
