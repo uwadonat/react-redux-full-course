51c996dacf99c7eeb37143de7bd1c56ab8110f6f
uwadonat <uwadonat@gmail.com>
2021-12-26 20:38:13 +0300

    Add ternary condition

--- src/components/component.js
+++ src/components/component.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Component1 = (props) => {
-  if (false) {
-    return <div>container div1</div>;
-  } else {
-    return <div>container div2</div>;
-  }
+  // if (false) {
+  //   return <div>container div1</div>;
+  // } else {
+  //   return <div>container div2</div>;
+  // }
+  return false ? <div>container div1</div> : <div>container div2</div>;
 };
 
 export default Component1;
