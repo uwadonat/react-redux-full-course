e834609b2431841a6265e605293cf44544497de0
uwadonat <uwadonat@gmail.com>
2021-12-27 19:42:32 +0300

    SetSate using form

--- src/container/form.js
+++ src/container/form.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  state = {
+    value: "",
+  };
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.value);
+  };
+
   render() {
-    return <div>My Form</div>;
+    return (
+      <div>
+        {this.state.value}
+        <form onSubmit={this.handleSubmit}>
+          <label name="name">my name:</label>
+          <input id="name" onChange={this.handleChange} type="text" />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
