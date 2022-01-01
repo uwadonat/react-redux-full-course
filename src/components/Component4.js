7e36432b33dc6245cbabc3793fa0d66cfc1e29f1
uwadonat <uwadonat@gmail.com>
2022-01-01 15:29:05 -0500

    Access state in componet4 from redux

--- src/components/Component4.js
+++ src/components/Component4.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+
 
 const Component4 = () => {
+    const statee = useSelector((state) => state.reducer1.stateProp1);
+console.log("state in componet4",statee)
     return(
         <div>
             Component4
